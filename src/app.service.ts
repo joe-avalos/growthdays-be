8231e76c1b0e8c51ae651ece8b474d00a6dfe621
Jose Avalos <jose.b.avalos@gmail.com>
2022-04-24 22:51:59 -0500

    main - sevice cluster

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,13 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `
+    Hello World!
+    ${process.env.DB_HOST}
+    ${process.env.DB_NAME}
+    ${process.env.DB_USER}
+    ${process.env.DB_PASSWORD}
+    ${process.env.DB_PORT}
+    `;
   }
 }
