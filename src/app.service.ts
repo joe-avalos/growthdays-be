8f42fbc5b8053eb0fc5c8caa9ceef2bfbba29085
Jose Avalos <jose.b.avalos@gmail.com>
2022-04-25 19:04:30 -0500

    main - remove env vars from homepage

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,11 +5,6 @@ export class AppService {
   getHello(): string {
     return `
     Hello World!
-    ${process.env.DB_HOST}
-    ${process.env.DB_NAME}
-    ${process.env.DB_USER}
-    ${process.env.DB_PASSWORD}
-    ${process.env.DB_PORT}
     `;
   }
 }
