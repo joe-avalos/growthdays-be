ccd54fda9ba394c3e93caf8839432debcaa8176d
Jose Avalos <jose.b.avalos@gmail.com>
2022-05-05 11:41:50 -0500

    main - seed undo

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
-  await app.listen(8080);
+  const server = await app.listen(80);
+  server.keepAliveTimeout = 65000
 }
 bootstrap();
